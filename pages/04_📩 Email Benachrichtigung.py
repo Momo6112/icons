95ed81247950e6713c11ddd5084b3ece77d5ef6a
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-23 17:45:44 +0200

    Update 04_📩 Email Benachrichtigung.py

--- pages/04_📩 Email Benachrichtigung.py
+++ pages/04_📩 Email Benachrichtigung.py
@@ -44,7 +44,7 @@ cursor = conn.cursor()
 def app(): 
     st.subheader("Benachrichtigung anfordern")
     anfragenlistebenutzer=[]
-    collll1,collll2=st.columns(2)
+    collll1,collll2=st.columns(4)
     with collll1:
       user=st.text_input("Benutzer:",st.session_state.name)
       richtigentabellen1=cursor.execute("Select anfragen.tabelle from anfragen where username=%s", [st.session_state.name])
