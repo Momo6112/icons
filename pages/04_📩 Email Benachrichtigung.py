afc6a738cf8b7ac1bfa1d95fdfe2c3de07185130
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-23 17:34:54 +0200

    Update 04_📩 Email Benachrichtigung.py

--- pages/04_📩 Email Benachrichtigung.py
+++ pages/04_📩 Email Benachrichtigung.py
@@ -43,6 +43,7 @@ cursor = conn.cursor()
 
 def app(): 
     st.subheader("Benachrichtigung anfordern")
+    anfragenlistebenutzer=[]
     user=st.text_input("Benutzer:",st.session_state.name)
     emailteil1=st.text_input("Emailnamen eingeben")
     emaildomains=["@gmail.com","@gmx.de","@web.de"]
