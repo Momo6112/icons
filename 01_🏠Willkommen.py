f7174e38d8d1912f74e456834b4481a1ed3bbfc4
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-24 15:11:00 +0200

    Update 01_🏠Willkommen.py

--- 01_🏠Willkommen.py
+++ 01_🏠Willkommen.py
@@ -19,14 +19,14 @@ import bcrypt
 import pandas as pd
 
 
-#conn = psycopg2.connect(host ="dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com",
-                        #database="dbticket", 
-                        #user="dbticket_user", 
-                        #password="Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy")
+conn = psycopg2.connect(host ="dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com",
+                        database="dbticket", 
+                        user="dbticket_user", 
+                        password="Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy")
 
-#engine = create_engine('postgresql://dbticket_user:Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy@dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com/dbticket')
-#global cursor 
-#cur = conn.cursor()
+engine = create_engine('postgresql://dbticket_user:Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy@dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com/dbticket')
+global cursor 
+cur = conn.cursor()
 
 
 def app():
