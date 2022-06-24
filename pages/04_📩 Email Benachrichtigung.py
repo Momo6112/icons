8a897d1e1e14bf3341478acab7ab0e430e0dbc8d
Momo6112 <106674519+Momo6112@users.noreply.github.com>
2022-06-24 18:25:21 +0200

    Update 04_📩 Email Benachrichtigung.py

--- pages/04_📩 Email Benachrichtigung.py
+++ pages/04_📩 Email Benachrichtigung.py
@@ -58,7 +58,10 @@ def app():
         for tabell in alleanfragen1:
             anfragenlistebenutzer.append(tabell[0])
       boxen1=st.selectbox("Für folgende Anfrage:", anfragenlistebenutzer)
-      tabell2=cursor.execute(f'SELECT * FROM {boxen1}')
+      tabell2 = cursor.execute("SELECT * from "boxen1"")
+      myresult=cursor.fetchall()
+      for row in myresult:
+        print(row)
       prei=cursor.fetchall()
       df_diagramm= pd.DataFrame(tabell2)
       st.table(df_diagramm)
