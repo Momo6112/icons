17c6e0ee1fc88896cdf7446116ccff514e4a7fa3
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-24 16:01:42 +0200

    Update 01_🏠Willkommen.py

--- 01_🏠Willkommen.py
+++ 01_🏠Willkommen.py
@@ -222,7 +222,7 @@ def app():
       sparpreis_zv=sparpreis_zv3.replace("ab","€")
 
       if "Verbindung liegt in der Vergangenheit" in sparpreis_zv1: 
-          print("Diese Verbindung liegt in der Vergangenheit. Wählen Sie eine andere Verbindung")
+          st.info("Diese Verbindung liegt in der Vergangenheit. Wählen Sie eine andere Verbindung")
 
       else: 
           if "THA" in art_zug_zv2:
@@ -250,7 +250,7 @@ def app():
                       (" ",'Einloggen', 'Registrieren'))
                       sbest=st.form_submit_button("Auswählen")
                   if option=="Einloggen":
-                    with col1:
+                    with coll1:
                       with st.form("log"):
                           loginn=st.text_input("Benutzername: ")
                           loginp=st.text_input("Passwort: ",type="password")
@@ -278,6 +278,104 @@ def app():
                                       st.session_state.passw=loginp
                       if best:
                           Login(loginn,loginp)
+                          wunsch=st.text_input("Anfrage speichern in :")
+                          tabe=''.join(wunsch)
+
+
+                          with st.form(key='form'):
+                            submit_buttonhome = st.form_submit_button(label='Bestätigen')
+
+                          def mehrereanfragen(user,wunsch):
+                                        tababfrage=cursor.execute("Select anfragen.tabelle From anfragen where username=%s and tabelle=%s",[user,wunsch])
+                                        if not cursor.fetchone():
+                                          result=pandas.DataFrame(columns=["username","tabelle"])   
+                                          result.loc[len(result)]=[benut,wunsch]
+                                          result.to_sql(name="anfragen", con=engine, if_exists="append")
+                                          result=result[0:0]
+                                          start=option
+                                          ziel=zielbahn
+                                          datum=losdatum.strftime("%d.%m.%Y") 
+                                          uhrzeit_stunde=str(uhrzeit_stunde1)
+                                          uhrzeit_minuten=str(uhrzeit_minuten1)
+
+                                          uhrzeit_minuten=str(uhrzeit_minuten1)
+                                          if alter_1 in range(15,5,-1):
+                                                alter="f"
+                                          else: 
+                                            if alter_1 in range(26,13,-1): 
+                                                    alter="y"
+                                            else:
+                                                    if alter_1 in range(64,26,-1):
+                                                        alter="e"
+                                                    else: 
+                                                        alter="s" 
+
+
+                                          if bahnkarteneu=="50":
+                                            bahnkarte="4"
+                                          else: 
+                                            if bahnkarteneu=="25":
+                                                    bahnkarte="2"
+                                            else: 
+                                                    bahnkarte="0"
+                                          while True:
+
+
+                                            url='https://reiseauskunft.bahn.de/bin/query.exe/dn?revia=yes&existOptimizePrice-deactivated=1&country=DEU&dbkanal_007=L01_S01_D001_qf-bahn-svb-kl2_lz03&start=1&protocol=https%3A&REQ0JourneyStopsS0A=1&S='+start+'&REQ0JourneyStopsSID=A%3D1%40O%3DM%C3%BCnchen+Hbf%40X%3D11558339%40Y%3D48140229%40U%3D80%40L%3D008000261%40B%3D1%40p%3D1652295202%40&REQ0JourneyStopsZ0A=1&Z='+ziel+'&REQ0JourneyStopsZID=A%3D1%40O%3DAachen+Hbf%40X%3D6091495%40Y%3D50767803%40U%3D80%40L%3D008000001%40B%3D1%40p%3D1652295202%40&date=Fr%2C+'+datum+'&time='+uhrzeit_stunde+'%3A'+uhrzeit_minuten+'&timesel=depart&returnDate=&returnTime=&returnTimesel=depart&optimize=0&auskunft_travelers_number=1&tariffTravellerType.1='+alter+'&tariffTravellerReductionClass.1='+bahnkarte+'&tariffClass=2&rtMode=DB-HYBRID&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21#hfsseq1|gl.0263982.1652621988'
+                                            source=requests.get(url)
+                                            soup = BeautifulSoup(source.text,"html.parser")
+
+                                            zugverbindungen=soup.find("div", class_= "overviewConnection")
+                                            zugverbindungen1=zugverbindungen.find("div", class_="connectionRoute")
+                                            station1=zugverbindungen1.find("div", class_="station first").get_text(strip=True)
+                                            station2=zugverbindungen1.find("div", class_="station stationDest").get_text(strip=True)
+                                            uhrzeit_zv1=zugverbindungen.find("div", class_= "connectionTimeSoll")
+                                            zeiten_zv1=uhrzeit_zv1.find("div", class_= "time").get_text(strip=True)
+                                            art_zug_zv1=soup.find("div", class_= "connectionData")
+                                            art_zug_zv2=art_zug_zv1.find("div", class_= "connectionBar").get_text(strip=True)
+                                            preis_zv1=zugverbindungen.find("div",class_="connectionAction").get_text(strip=True)
+                                            sparpreis_zv2=preis_zv1.replace("ab","")
+                                            sparpreis_zv1=sparpreis_zv2.replace("Rückfahrt hinzufügen","")
+                                            sparpreis_zv=sparpreis_zv1.replace("€","")
+                                            sparpreis_ohne_punkt=sparpreis_zv.replace(",",".")
+                                            preis_float=float(sparpreis_ohne_punkt)
+
+
+
+                                            if "Verbindung liegt in der Vergangenheit" in sparpreis_zv1: 
+                                              st.info("Diese Verbindung liegt in der Vergangenheit. Wählen Sie eine andere Verbindung")
+                                              break
+
+                                            else: 
+                                                if "THA" in art_zug_zv2:
+
+                                                  st.info("Diese Zugverbindung wird nicht von uns unterstüzt. Bitte wählen Sie eine Verbindung der Züge von der DB.")
+                                                  break
+
+                                                else: 
+                                                    if "VRS-Tarif" in sparpreis_zv1:
+
+                                                      st.info("Hier ist kein Vergleich notwendig, da diese Verbindung zu VRS-Tarifen angeboten wird.")
+                                                      break 
+
+                                                    else:
+
+                                                          anfrage_tage=time.strftime("%d.%m")
+                                                          anfrage_zeit=time.strftime("%H:%M")
+                                                          anfrage_komplett=time.strftime("%d.%m. %H:%M")
+                                                          result=pandas.DataFrame(columns=["anfrage_tag","anfrage_uhrzeit","anfrage_komplett","startbahnhof", "zielbahnhof","fahrzeit","preis"])
+                                                          result.loc[len(result)]=[anfrage_tage,anfrage_zeit, anfrage_komplett,station1,station2,zeiten_zv1,preis_float]
+                                                          result.to_sql(name=tabe, con=engine, if_exists="append" )
+                                                          result=result[0:0]
+
+
+                                                    sleep(18)
+
+                                            st.success("Sie haben die Anfrage erfolgreich gestellt")
+                                          else:
+                                             st.warning("Sie haben bereits eine solche Tabelle angelegt")
+                          if submit_buttonhome:
+                            mehrereanfragen(benut,tabe)
                   if option=="Registrieren":
 
 
