2e8eb637588be7f42a42d15f4be795be852929cf
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-24 13:45:57 +0200

    Update 03_📈 Diagramm & Preisvorhersage.py

--- pages/03_📈 Diagramm & Preisvorhersage.py
+++ pages/03_📈 Diagramm & Preisvorhersage.py
@@ -25,10 +25,10 @@ cursor = conn.cursor()
 def app():
   coll1,coll2,coll3,coll4=st.columns(4)
   
- with coll1:
+  with coll1:
     loginname=st.text_input("Benutzer: ",st.session_state.name)
   
- with coll2:
+  with coll2:
     loginpassw=st.text_input("Passwort:",st.session_state.passw)
     anfragenlistebenutzer=[]
     abfrage = cursor.execute("SELECT login.username FROM login WHERE username=%s", [loginname])
