e24eb4f507999f56fe0290cb9dd883c328e1e952
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-24 17:28:48 +0200

    Update 01_🏠Willkommen.py

--- 01_🏠Willkommen.py
+++ 01_🏠Willkommen.py
@@ -371,7 +371,7 @@ def app():
                                     
 
                                   
-                  if best:
+                    if best:
                       Login(loginn,loginp)
                       if 'willen' not in st.session_state:
                           st.session_state.willen= True
