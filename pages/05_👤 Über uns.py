6a0f20269a031d248ccfe0e81a895196c1fd2765
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-24 23:42:49 +0200

    Update 05_👤 Über uns.py

--- pages/05_👤 Über uns.py
+++ pages/05_👤 Über uns.py
@@ -46,9 +46,10 @@ def load_lottieurl(url: str):
 #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
 #lottie_hello = load_lottieurl(lottie_url_hello)
 #st_lottie(lottie_hello, key="hello")
+
 def app():
-   st.subheader("Hallo!")
-   strings= ("Wir sind Artur, Katja, Anna-Maria, Muhammet, Sven und Lina."
+    st.subheader("Hallo!")
+    strings= ("Wir sind Artur, Katja, Anna-Maria, Muhammet, Sven und Lina."
                "\n"
                "\n"
                "Wir sind Studierende der FH Aachen und haben im Zuge des Vertiefungsmoduls Digital Lab diese Website ins Leben gerufen.""\n" "\n"
@@ -73,11 +74,12 @@ def app():
                "Damit soll vermieden werden, dass ihr selbst ständig eure Verbindung aktualisieren müsst." "\n"
                "So spart ihr mit DB-Price-APP  im Idealfall nicht nur Geld sondern auch noch Zeit und Nerven ;-)"  
                "\n" )
-               st.write(strings)
-   lottie_url_hello = "https://assets9.lottiefiles.com/packages/lf20_p4kr3lub.json"
+    st.write(strings)
+    lottie_url_hello = "https://assets9.lottiefiles.com/packages/lf20_p4kr3lub.json"
  #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
-   lottie_hello = load_lottieurl(lottie_url_hello)
-   st_lottie(lottie_hello, key="hello")
+    lottie_hello = load_lottieurl(lottie_url_hello)
+    st_lottie(lottie_hello, key="hello")
+
 app()
 
 def app2():
@@ -102,8 +104,9 @@ def app2():
                "\n"
                "Lina Koppany: 	lina.koppany@alumni.fh-Aachen.de""\n"
             )
-   st.write(strings2)
+     st.write(strings2)
 app2()
    
 load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_p4kr3lub.json")
+
 
