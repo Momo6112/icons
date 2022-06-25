da8644c16ff2e44f298c32f23161aa4f1feb23fe
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-25 03:19:38 +0200

    Update 06_🚪 Logout.py

--- pages/06_🚪 Logout.py
+++ pages/06_🚪 Logout.py
@@ -17,7 +17,7 @@ def load_lottieurl2(url: str):
   lottie_url_hello2 = "https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json"
  #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
   lottie_hello2 = load_lottieurl2(lottie_url_hello2)
-  st_lottie(lottie_hello2, key="hello") 
+  st_lottie(lottie_hello2, key="bye") 
   
 
 def app():
