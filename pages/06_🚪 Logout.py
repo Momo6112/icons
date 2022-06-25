51ceaa63a0daec36bc05bc5c6b71aff773183a81
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-25 03:25:38 +0200

    Update 06_🚪 Logout.py

--- pages/06_🚪 Logout.py
+++ pages/06_🚪 Logout.py
@@ -18,7 +18,7 @@ def load_lottieurl2(url: str):
  #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
     lottie_hello2 = load_lottieurl2(lottie_url_hello2)
     st_lottie(lottie_hello2, key="hello") 
-  
+load_lottieurl2("https://assets4.lottiefiles.com/packages/lf20_7mibdcvp.json")  
 
 def app():
     st.write(st.session_state.name,
@@ -35,4 +35,4 @@ def app():
         st.info("Nicht abgemeldet")
 app()
  
-load_lottieurl2("https://assets4.lottiefiles.com/packages/lf20_7mibdcvp.json")
+
