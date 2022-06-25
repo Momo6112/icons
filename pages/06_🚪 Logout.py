e4095a1dd7ada9a5ff253b034ff861590d2a4536
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-25 03:10:45 +0200

    Update 06_🚪 Logout.py

--- pages/06_🚪 Logout.py
+++ pages/06_🚪 Logout.py
@@ -9,6 +9,11 @@ import streamlit_multipage
 from streamlit_multipage.multipage import MultiPage
 import streamlit as st
 from streamlit_option_menu import option_menu
+def load_lottieurl2(url: str):
+    f = requests.get(url)
+    if f.status_code != 200:
+     return None
+     return r.json()
 
 col1,col2=st.columns(2)
 
@@ -29,16 +34,11 @@ with col1:
           st.info("Nicht abgemeldet")
 app()
 with col2:
-   def load_lottieurl2(url: str):
-      f = requests.get(url)
-      if f.status_code != 200:
-       return None
-       return r.json()
-       lottie_url_hello2 = "https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json"
+ 
+  lottie_url_hello2 = "https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json"
  #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
-       lottie_hello2 = load_lottieurl2(lottie_url_hello2)
-       st_lottie(lottie_hello2, key="hello")
 
+
+lottie_hello2 = load_lottieurl2(lottie_url_hello2)
+st_lottie(lottie_hello2, key="hello")
 load_lottieurl2("https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json")
-        
-#https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json
