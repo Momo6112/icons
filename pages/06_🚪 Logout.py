d9482d21267ea754d197ae66263ae85edefb738b
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-25 02:59:06 +0200

    Update 06_🚪 Logout.py

--- pages/06_🚪 Logout.py
+++ pages/06_🚪 Logout.py
@@ -9,29 +9,35 @@ import streamlit_multipage
 from streamlit_multipage.multipage import MultiPage
 import streamlit as st
 from streamlit_option_menu import option_menu
-def load_lottieurl2(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-def app():
-    st.write(st.session_state.name,
-            "Möchten Sie sich wirklich ausloggen?")
-    with st.form("button"):
-        ja=st.form_submit_button(label="Ja")
-        nein=st.form_submit_button(label="Nein")
+
+col1,col2=st.colums(2)
+with col1: 
+  def app():
+      st.write(st.session_state.name,
+              "Möchten Sie sich wirklich ausloggen?")
+      with st.form("button"):
+          ja=st.form_submit_button(label="Ja")
+          nein=st.form_submit_button(label="Nein")
         
-    if ja:
-        del st.session_state.name
-        st.success("Sie haben sich erfolgreich ausgeloggt")
-        lottie_url_hello2 = "https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json"
- #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
-        lottie_hello2 = load_lottieurl(lottie_url_hello2)
-        st_lottie(lottie_hello2, key="hello")
+      if ja:
+          del st.session_state.name
+          st.success("Sie haben sich erfolgreich ausgeloggt")
+         
         
-    if nein:
-        st.info("Nicht abgemeldet")
+      if nein:
+          st.info("Nicht abgemeldet")
 app()
-load_lottieurl2("https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json")
+with col2:
+    def load_lottieurl2(url: str):
+      r = requests.get(url)
+      if r.status_code != 200:
+          return None
+      return r.json()
+      lottie_url_hello2 = "https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json"
+ #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
+     lottie_hello2 = load_lottieurl(lottie_url_hello2)
+     st_lottie(lottie_hello2, key="hello")
+   
+    load_lottieurl2("https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json")
         
 #https://assets9.lottiefiles.com/packages/lf20_alpjfedu.json
