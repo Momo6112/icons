ad8adc6b4150d6b89c25626e6f26a25c251969aa
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 11:23:44 +0200

    Update 01_🚂Willkommen.py

--- 01_🚂Willkommen.py
+++ 01_🚂Willkommen.py
@@ -273,6 +273,7 @@ def app():
                                   st.success("Du hast Dich erfolgreich angemeldet")
                                   def mehrereanfragen(loginn,loginp):
                                               tababfrage=cursor.execute("Select anfragen.tabelle From anfragen where username=%s and tabelle=%s",[loginn,wunsch])
+                                              st.info(tababfrage)
                                               if not cursor.fetchone():
                                                 result=pandas.DataFrame(columns=["username","tabelle"])   
                                                 result.loc[len(result)]=[benut,wunsch]
