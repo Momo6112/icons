bdd3bb260896c3f129312a55dd338955de476d72
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 09:49:01 +0200

    Update 01_🚂Willkommen.py

--- 01_🚂Willkommen.py
+++ 01_🚂Willkommen.py
@@ -29,7 +29,7 @@ global cursor
 cur = conn.cursor()
 
 def app():
-    st.set_page_config("DB","house",layout="wide")
+    st.set_page_config("DB-Price-App","train",layout="wide")
     st.title("DB Price App")
     #st.info("Um diese App vollständig zu benutzen, müssen Sie sich auf der Willkommen-Seite eingeloggt haben.")
     #st.warning("Ansonsten werden Sie einige Funktionen nicht durchführen können.")
